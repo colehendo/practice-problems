99448feac090bc69e6d0ea8f7075b4b37b44b1ea
Cole Henderson <henderson.cole98@gmail.com>
2022-11-11 11:56:19 -0800

    updated longest substring without repeating characters

--- python/longest-substring-without-repeating-characters/solution.py
+++ python/longest-substring-without-repeating-characters/solution.py
@@ -3,19 +3,19 @@ from collections import defaultdict
 
 class Solution:
     def compile_substrings(self, s: str, current_max: int) -> int:
-        substring = ""
+        substring_len = 0
         chars_found = defaultdict(lambda: None)
 
         for index, char in enumerate(s):
             if chars_found[char] is None:
-                substring += char
+                substring_len += 1
                 chars_found[char] = index
             else:
                 return self.compile_substrings(
-                    s[chars_found[char] + 1 :], max(current_max, len(substring))
+                    s[chars_found[char] + 1 :], max(current_max, substring_len)
                 )
 
-        return max(current_max, len(substring))
+        return max(current_max, substring_len)
 
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_substring = self.compile_substrings(s, 0)
